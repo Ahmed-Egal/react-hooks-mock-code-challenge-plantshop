b7f214bfa18432fb7747d80d2115934cfaea2b4b
Ahmed-Egal <egalosman101@gmail.com>
2023-08-05 13:23:15 +0300

    Plant list is showing when you start the page

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+  const [plants, setPlants] = useState([]);
+
+  
+
+  useEffect (() => {
+    const fetchData = async () => {
+      try{
+        const response = await fetch('http://localhost:6001/plants');
+        const data = await response.json();
+        setPlants(data);
+      } catch (error){
+        console.log('Error fetching data :', error);
+      }
+    };
+    fetchData();
+  }, [])
+
+
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plants.map((plant) => (
+      <PlantCard key={plant.id} plant={plant} />
+    ))}
+    </ul>
   );
 }
 
